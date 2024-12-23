faa7a64ca10368e7969db366516c1a64f92a7db9
Navrose <navrosesinghjohal@gmail.com>
2024-12-23 13:56:40 -0500

    Component class and decorators

--- src/app/modules.ts
+++ src/app/modules.ts
@@ -10,3 +10,14 @@
 //     ]
 // }
 // }
+// this decorator take a configuration object as the only argument
+// one special property is bootstrap which tells angular which component to load
+// only the root module should have the bootstrap property in module architecture
+
+// Angular Components
+// These are the main building blocks in all Angular apps
+// four parts - html,  css, Tyepscript, component selector
+// reusablility -- @Component decorator also takes only a configuration object as it's only argument
+
+// Component class itself
+
