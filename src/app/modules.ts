ced198c805055e99a34e3292fb3203f03cf1f1ef
Navrose <navrosesinghjohal@gmail.com>
2024-12-23 13:53:11 -0500

    MOdules and standalone architecture

---
+++ src/app/modules.ts
@@ -0,0 +1,12 @@
+// collections of fiels that work together
+// Standalone componenets - let you use files wihtout modules
+// to declare a class as a module use @NgModule decorator
+// @NgModule{
+//     {declarations:[
+//         AppComponent
+//     ],
+//     imports:[
+
+//     ]
+// }
+// }
