e7632c6f1a62ac280bb09564fc5660810c183d8d
Navrose <navrosesinghjohal@gmail.com>
2024-12-23 13:47:19 -0500

    main.ts added decorators

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+// import { AppModule } from './app/app.module';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+// Decorators are functions that return functions - tell angular what the code should do and how angular should inject
+// that code into other part of the application
+// they are marked using "@" symbol 
+// invoked at runtime with arguments
+// eg. @NgModule(), @Component(), @Injectable, @Pipe() , @DIredctive(), inputoutput
+// errors - missing, parentheses, properties , values and semicolon
